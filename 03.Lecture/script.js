349be09e18a5dbe80088ed396e335f63a3b901d0
allseenn <allseen@live.ru>
2024-08-29 11:44:20 +0300

    03

---
+++ 03.Lecture/script.js
@@ -0,0 +1,67 @@
+// Родительский объект робот-пылесос.
+const VacuumCleaner = {
+    Model: "vacuum cleaner",
+    counterOfStarts: 0,
+    isFull: false,
+    isObstacle: false,
+    startCleaning: function () {
+        this.counterOfStarts++;
+        // Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of VacuumCleaner');
+        console.log('I am cleaning... I have been started: ', this.counterOfStarts, 'times.');
+    },
+    goCharge: function () {
+        // Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of VacuumCleaner');
+        console.log('I am going to charge...');
+    }
+};
+
+// Дочерний объект робот-пылесос.
+const DancingSeries = {
+    // Объявляем новые свойства и переопределить свойство model.
+    model: "dancing series",
+    power: 200,
+    batterySize: 2100,
+    boxSize: 0.5,
+    workTime: 45,
+    isUVLampOn: false,
+    // Добавляем новый метод.
+    switchUVLamp: function () {
+        // Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of DancingSeries');
+        this.isUVLampOn = !this.isUVLampOn;
+        console.log(`UV lamp is ${this.isUVLampOn ? 'working' : 'not working'}.`);
+    },
+    // Делаем ссылку на прототип от родителя.
+    __proto__: VacuumCleaner,
+};
+
+// Дочерне-дочерний объект робот-пылесос.
+const Samba = {
+    // Обновляем свойства под конкретную модель.
+    model: "Samba-1",
+    power: 250,
+    batterySize: 2500,
+    workTime: 50,
+    // Делаем ссылку на прототип от родителя.
+    __proto__: DancingSeries,
+};
+
+// Объект робот-пылесос.
+const Djaiv = {
+    // Обновляем свойства под конкретную модель.
+    model: "Djaiv-1",
+    power: 250,
+    batterySize: 2500,
+    workTime: 50,
+    // Переопределим метод startCleaning.
+    startCleaning: function () {
+        this.counterOfStarts++;
+        // Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of Djaiv');
+        console.log('I am Djaiv, and I am cleaning... I have been started: ', this.counterOfStarts, 'times.');
+    },
+    // Делаем ссылку на прототип от родителя.
+    __proto__: DancingSeries,
+};
