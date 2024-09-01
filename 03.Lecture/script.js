a4391f39de68a327291cdcddfe22101b8111778b
allseenn <allseen@live.ru>
2024-09-01 19:47:34 +0000

    03.Tasks

--- 03.Lecture/script.js
+++ 03.Lecture/script.js
@@ -37,16 +37,17 @@ const DancingSeries = {
     __proto__: VacuumCleaner,
 };
 
-// Дочерне-дочерний объект робот-пылесос.
-const Samba = {
-    // Обновляем свойства под конкретную модель.
-    model: "Samba-1",
-    power: 250,
-    batterySize: 2500,
-    workTime: 50,
-    // Делаем ссылку на прототип от родителя.
-    __proto__: DancingSeries,
-};
+// // Дочерне-дочерний объект робот-пылесос.
+// const Samba = {
+//     // Обновляем свойства под конкретную модель.
+//     model: "Samba-1",
+//     power: 250,
+//     batterySize: 2500,
+//     workTime: 50,
+//     // Делаем ссылку на прототип от родителя.
+//     __proto__: DancingSeries,
+
+// };
 
 // Объект робот-пылесос.
 const Djaiv = {
@@ -65,3 +66,63 @@ const Djaiv = {
     // Делаем ссылку на прототип от родителя.
     __proto__: DancingSeries,
 };
+
+// Объект робот-пылесос.
+const MusicSeries = {
+    // Объявляем новые свойства и переопределяем свойство model.
+    model: "music series",
+    power: 200,
+    batterySize: 2100,
+    boxSize: 0.5,
+    workTime: 45,
+    // Добавляем новый метод.
+    startWipe: function () {
+        // Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of MusicSeries');
+        console.log('I am starting to wipe the floor...');
+    },
+    // Делаем ссылку на прототип от родителя.
+    __proto__: VacuumCleaner,
+};
+
+// Объект робот-пылесос.
+const Blues = {
+    // Обновляем свойства под конкретную модель.
+    model: "Bluees-1",
+    power: 250,
+    batterySize: 2500,
+    workTime: 50,
+};
+// Установим прототип для робота.
+Object.setPrototypeOf(Blues, MusicSeries);
+
+if (Object.getPrototypeOf(Djaiv).model === 'dancing series') {
+    Djaiv.startCleaning(); //
+}
+if (Object.getPrototypeOf(Blues).model === 'music series') {
+    Blues.startWipe(); //
+}
+
+function Samba(serailNumber) {
+    // Создаем свойства объекта, используя this.
+    this.serialNumber = serailNumber;
+    this.model = "Samba-1";
+    this.power = 250;
+    this.batterySize = 2500;
+    this.workTime = 50;
+    // Делаем ссылку на прототип от родителя.
+    this.__proto__ = DancingSeries;
+}
+
+// Создадим экземпляр нового объекта.
+const Samba1 = new Samba(1014778);
+console.log(Samba1);
+Samba1.startCleaning();
+
+const robots = [];
+for (let index = 0; index < 10; index++) {
+    // Создадим экземпляр нового объекта и добавляем его в массив наших роботов, каждый с уникальным серийным номером.
+    robots.push(new Samba(index));
+}
+
+console.log(robots.constructor.prototype);
